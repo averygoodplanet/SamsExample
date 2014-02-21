cd652359035fa384391b084555ac484f3b76eef5
averygoodplanet <averygoodplanet@gmail.com>
2014-02-21 15:02:53 -0600

    commented users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  #instance variables in the controller become available to corresponding view
+  # e.g. for users#index  in views/users/index
+
   def index
     @users = User.all
   end
